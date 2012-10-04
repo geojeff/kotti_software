8ccba0acc53cf3b81c8dae6469f70f6388064b23
geojeff <geojeff@me.com>
2012-10-04 04:21:55 -0500

    Added creation of SoftwareCollection in root, then test for items and settings.

--- kotti_software/tests/test_views.py
+++ kotti_software/tests/test_views.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
 import os
+import kotti
 from kotti.resources import get_root
-from kotti.resources import Image
 from kotti.testing import DummyRequest
 from kotti.testing import UnitTestBase
 from kotti_software.resources import SoftwareCollection
@@ -30,18 +30,48 @@ class ViewsTests(UnitTestBase):
         softwareproject = root['softwareproject'] = SoftwareProject()
         view = SoftwareCollectionView(root, DummyRequest()).view()
 
-        assert view is not None
-        #assert ('projects' in view) and (len(view['projects']) == 1)
-        assert ('settings' in view) and \
-                ('use_batching' in view['settings']) and \
-                (view['settings']['use_batching'] is True)
-        assert ('settings' in view) and \
-                ('pagesize' in view['settings']) and \
-                (view['settings']['pagesize'] == 5)
-        assert ('settings' in view) and \
-                ('use_auto_batching' in view['settings']) and \
-                (view['settings']['use_auto_batching'] is True)
-        assert ('settings' in view) and \
-                ('link_headline_overview' in view['settings']) and \
-                (view['settings']['link_headline_overview'] is True)
+        assert softwareproject is not None
 
+        assert view is not None
+
+        assert ('items' in view) and (len(view['items']) == 1)
+
+        assert ('api' in view) \
+                and (type(view['api']) is kotti.views.util.TemplateAPI)
+
+        assert ('settings' in view) \
+                 and ('use_batching' in view['settings']) \
+                 and (view['settings']['use_batching'] is True)
+        assert ('settings' in view) \
+                and ('pagesize' in view['settings']) \
+                and (view['settings']['pagesize'] == 5)
+        assert ('settings' in view) \
+                and ('use_auto_batching' in view['settings']) \
+                and (view['settings']['use_auto_batching'] is True)
+        assert ('settings' in view) \
+                and ('link_headline_overview' in view['settings']) \
+                and (view['settings']['link_headline_overview'] is True)
+
+        softwarecollection = root['softwarecollection'] = SoftwareCollection()
+        view = SoftwareCollectionView(root['softwarecollection'],
+                                      DummyRequest()).view()
+
+        assert view is not None
+
+        assert ('items' in view) and (len(view['items']) == 0)
+
+        assert ('settings' in view) \
+                 and ('use_batching' in view['settings']) \
+                 and (view['settings']['use_batching'] is True)
+        assert ('settings' in view) \
+                and ('pagesize' in view['settings']) \
+                and (view['settings']['pagesize'] == 5)
+        assert ('settings' in view) \
+                and ('use_auto_batching' in view['settings']) \
+                and (view['settings']['use_auto_batching'] is True)
+        assert ('settings' in view) \
+                and ('link_headline_overview' in view['settings']) \
+                and (view['settings']['link_headline_overview'] is True)
+        assert (('settings' in view) \
+                 and ('use_batching' in view['settings']) \
+                 and (view['settings']['use_batching'] is True))
