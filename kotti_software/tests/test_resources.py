ccd58b16e579c3babcdb51e6dead5eb492df86fd
geojeff <geojeff@me.com>
2012-10-04 09:55:45 -0500

    Moved tests for resources.py into one file, for both unit and functional tests. Added coverage for instantiating a SoftwareProject with json_url.

--- kotti_software/tests/test_resources.py
+++ kotti_software/tests/test_resources.py
@@ -1,16 +1,22 @@
 # -*- coding: utf-8 -*-
 
+from pyramid.threadlocal import get_current_registry
+
 from kotti.resources import get_root
+
 from kotti.testing import DummyRequest
 from kotti.testing import UnitTestBase
+from kotti.testing import FunctionalTestBase
+from kotti.testing import testing_db_url
+
+from kotti_software import collection_settings
 from kotti_software.resources import SoftwareCollection
 from kotti_software.resources import SoftwareProject
 
 
-class ResourcesTests(UnitTestBase):
+class UnitTests(UnitTestBase):
 
     def test_software_collection(self):
-
         root = get_root()
         software_collection = SoftwareCollection()
         assert software_collection.type_info.addable(root, DummyRequest()) is True
@@ -26,3 +32,63 @@ class ResourcesTests(UnitTestBase):
         software_collection['software_project'] = software_project
 
         assert len(software_collection.values()) == 1
+
+    def test_software_project(self):
+        root = get_root()
+        software_collection = SoftwareCollection()
+        root['software_collection'] = software_collection
+
+        software_project = SoftwareProject(
+                json_url="https://raw.github.com/geojeff/kotti_software/master/json")
+
+        software_collection['software_project'] = software_project
+
+        assert len(software_collection.values()) == 1
+
+
+class FunctionalTests(FunctionalTestBase):
+
+    def setUp(self, **kwargs):
+        self.settings = {'kotti.configurators': 'kotti_software.kotti_configure',
+                         'sqlalchemy.url': testing_db_url(),
+                         'kotti.secret': 'dude',
+                         'kotti_software.collection_settings.pagesize': '5'}
+        super(FunctionalTests, self).setUp(**self.settings)
+
+    def test_asset_overrides(self):
+        from kotti import main
+        self.settings['kotti_software.asset_overrides'] = 'kotti_software:hello_world/'
+        main({}, **self.settings)
+
+    def test_softwarecollection_default_settings(self):
+        b_settings = collection_settings()
+        assert b_settings['use_batching'] == True
+        assert b_settings['pagesize'] == 5
+        assert b_settings['use_auto_batching'] == True
+        assert b_settings['link_headline_overview'] == True
+
+    def test_softwarecollection_change_settings(self):
+        settings = get_current_registry().settings
+        settings['kotti_software.collection_settings.use_batching'] = u'false'
+        settings['kotti_software.collection_settings.pagesize'] = u'2'
+        settings['kotti_software.collection_settings.use_auto_batching'] = u'false'
+        settings['kotti_software.collection_settings.link_headline_overview'] = u'false'
+
+        b_settings = collection_settings()
+        assert b_settings['use_batching'] == False
+        assert b_settings['pagesize'] == 2
+        assert b_settings['use_auto_batching'] == False
+        assert b_settings['link_headline_overview'] == False
+
+    def test_softwarecollection_wrong_settings(self):
+        settings = get_current_registry().settings
+        settings['kotti_software.collection_settings.use_batching'] = u'blibs'
+        settings['kotti_software.collection_settings.pagesize'] = u'blabs'
+        settings['kotti_software.collection_settings.use_auto_batching'] = u'blubs'
+        settings['kotti_software.collection_settings.link_headline_overview'] = u'blobs'
+
+        b_settings = collection_settings()
+        assert b_settings['use_batching'] == False
+        assert b_settings['pagesize'] == 5
+        assert b_settings['use_auto_batching'] == False
+        assert b_settings['link_headline_overview'] == False
