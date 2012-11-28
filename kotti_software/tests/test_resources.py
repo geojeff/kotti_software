00169299bdf8d5c987b4a9b9ebaee64fd5b5283d
geojeff <geojeff@me.com>
2012-11-28 09:33:53 -0600

    First converstion of functional tests to use funcargs.

--- kotti_software/tests/test_resources.py
+++ kotti_software/tests/test_resources.py
@@ -5,7 +5,6 @@ from pyramid.threadlocal import get_current_registry
 from kotti.resources import get_root
 
 from kotti.testing import DummyRequest
-from kotti.testing import FunctionalTestBase
 from kotti.testing import testing_db_url
 
 from kotti_software import collection_settings
@@ -129,50 +128,49 @@ def test_software_project_pypi_overwriting(db_session):
         pypi_url="http://pypi.python.org/pypi/Kotti/json",
         desc_handling_choice='use_pypi_description')
 
+# Functional Tests
 
-class FunctionalTests(FunctionalTestBase):
+def test_asset_overrides(db_session):
+    from kotti import main
+    settings = {'kotti.configurators': 'kotti_software.kotti_configure',
+                'sqlalchemy.url': testing_db_url(),
+                'kotti.secret': 'dude',
+                'kotti_software.collection_settings.pagesize': '10'}
+    settings['kotti_software.asset_overrides'] = 'kotti_software:hello_world/'
+    main({}, **settings)
 
-    def setUp(self, **kwargs):
-        self.settings = {'kotti.configurators': 'kotti_software.kotti_configure',
-                         'sqlalchemy.url': testing_db_url(),
-                         'kotti.secret': 'dude',
-                         'kotti_software.collection_settings.pagesize': '5'}
-        super(FunctionalTests, self).setUp(**self.settings)
 
-    def test_asset_overrides(self):
-        from kotti import main
-        self.settings['kotti_software.asset_overrides'] = 'kotti_software:hello_world/'
-        main({}, **self.settings)
+def test_softwarecollection_default_settings(db_session):
+    b_settings = collection_settings()
+    assert b_settings['use_batching'] is True
+    assert b_settings['pagesize'] == 5
+    assert b_settings['use_auto_batching'] is True
+    assert b_settings['link_headline_overview'] is True
 
-    def test_softwarecollection_default_settings(self):
-        b_settings = collection_settings()
-        assert b_settings['use_batching'] is True
-        assert b_settings['pagesize'] == 5
-        assert b_settings['use_auto_batching'] is True
-        assert b_settings['link_headline_overview'] is True
 
-    def test_softwarecollection_change_settings(self):
-        settings = get_current_registry().settings
-        settings['kotti_software.collection_settings.use_batching'] = u'false'
-        settings['kotti_software.collection_settings.pagesize'] = u'2'
-        settings['kotti_software.collection_settings.use_auto_batching'] = u'false'
-        settings['kotti_software.collection_settings.link_headline_overview'] = u'false'
+def test_softwarecollection_change_settings(db_session):
+    settings = get_current_registry().settings
+    settings['kotti_software.collection_settings.use_batching'] = u'false'
+    settings['kotti_software.collection_settings.pagesize'] = u'2'
+    settings['kotti_software.collection_settings.use_auto_batching'] = u'false'
+    settings['kotti_software.collection_settings.link_headline_overview'] = u'false'
 
-        b_settings = collection_settings()
-        assert b_settings['use_batching'] is False
-        assert b_settings['pagesize'] == 2
-        assert b_settings['use_auto_batching'] is False
-        assert b_settings['link_headline_overview'] is False
+    b_settings = collection_settings()
+    assert b_settings['use_batching'] is False
+    assert b_settings['pagesize'] == 2
+    assert b_settings['use_auto_batching'] is False
+    assert b_settings['link_headline_overview'] is False
 
-    def test_softwarecollection_wrong_settings(self):
-        settings = get_current_registry().settings
-        settings['kotti_software.collection_settings.use_batching'] = u'blibs'
-        settings['kotti_software.collection_settings.pagesize'] = u'blabs'
-        settings['kotti_software.collection_settings.use_auto_batching'] = u'blubs'
-        settings['kotti_software.collection_settings.link_headline_overview'] = u'blobs'
 
-        b_settings = collection_settings()
-        assert b_settings['use_batching'] is False
-        assert b_settings['pagesize'] == 5
-        assert b_settings['use_auto_batching'] is False
-        assert b_settings['link_headline_overview'] is False
+def test_softwarecollection_wrong_settings(db_session):
+    settings = get_current_registry().settings
+    settings['kotti_software.collection_settings.use_batching'] = u'blibs'
+    settings['kotti_software.collection_settings.pagesize'] = u'blabs'
+    settings['kotti_software.collection_settings.use_auto_batching'] = u'blubs'
+    settings['kotti_software.collection_settings.link_headline_overview'] = u'blobs'
+
+    b_settings = collection_settings()
+    assert b_settings['use_batching'] is False
+    assert b_settings['pagesize'] == 5
+    assert b_settings['use_auto_batching'] is False
+    assert b_settings['link_headline_overview'] is False
